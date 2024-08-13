5b26e466e81aa75698b449b565c82aa390957a0d
abds4816 <abds4816@gmail.com>
2024-08-13 11:32:08 +0100

    initial commit

--- src/components/edit-note.tsx
+++ src/components/edit-note.tsx
@@ -94,7 +94,7 @@ const EditNote: FC<EditNoteProps> = ({ data }) => {
         />
         <Button type="submit" disabled={isLoading} className="w-full">
           {isLoading && <Loader2 className="size-4 animate-spin" />}
-          <span>update note</span>
+          <span>updatenote</span>
         </Button>
       </form>
     </Form>
